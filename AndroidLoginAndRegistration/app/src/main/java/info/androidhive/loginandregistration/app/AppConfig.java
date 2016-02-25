9e7c57ff19bc17d24fac7ab24a48e12f2e639b29
easternmie <mhelmi.r@gmail.com>
2016-02-26 02:51:04 +0800

    updated PUT method for user update function

--- AndroidLoginAndRegistration/app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
+++ AndroidLoginAndRegistration/app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -1,15 +1,18 @@
 package info.androidhive.loginandregistration.app;
 
 public class AppConfig {
+	public static final String  HOST = "http://10.9.169.165/";
+
+
 	// Server user login url
 	//public static String URL_LOGIN = "http://192.168.0.102:8888/android_login_api/login.php";
-	public static String URL_LOGIN = "http://10.9.169.165/task_manager/v1/login";
+	public static String URL_LOGIN = HOST +  "task_manager/v1/login";
     //public static String URL_LOGIN = "http://192.168.0.103/task_manager/v1/login";
     //public static String URL_LOGIN = "http://192.168.15.72/task_manager/v1/login";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://10.9.169.165/task_manager/v1/register";
+	public static String URL_REGISTER = HOST +  "task_manager/v1/register";
 
     // Server user update url
-    public static String URL_UPDATE = "http://10.9.169.165/task_manager/v1/user";
+    public static String URL_UPDATE = HOST + "task_manager/v1/user";
 }
